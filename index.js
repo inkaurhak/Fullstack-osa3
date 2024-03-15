ffa6981cf725e64c2c4f616a4619ad38ef622ef8
Inka Häkkänen <inkahakkanenn@gmail.com>
2024-03-15 14:32:34 +0200

    3.19

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -39,15 +41,8 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-  
-    if (body.name === undefined) {
-        return response.status(400).json({ error: 'name missing' })
-    }
-    else if (body.number === undefined) {
-        return response.status(400).json({ error: 'number missing' })
-    }
   
     const person = new Person({
         name: body.name,
@@ -57,6 +52,7 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
   })
   
 
@@ -83,14 +79,13 @@ app.get('/info', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const { name, number } = request.body
 
-    const person = {
-      name: body.name,
-      number: body.number
-    }
-
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    Person.findByIdAndUpdate(
+      request.params.id, 
+      { name, number }, 
+      { new: true, runValidators: true, context: 'query' }
+      )
       .then(updatedPerson => {
         response.json(updatedPerson)
       })
