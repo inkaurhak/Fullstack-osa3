052b563013f8a15b35a893323b7b090feabc6e80
Inka Häkkänen <inkahakkanenn@gmail.com>
2024-03-15 15:43:17 +0200

    number validator

--- models/person.js
+++ models/person.js
@@ -21,6 +21,12 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String, 
+        minlength: 8,
+        validate: {
+            validator: function(v) {
+                return /\d{2,3}-\d{5,}/.test(v)
+            }
+        },
         required: true
     }
 })
