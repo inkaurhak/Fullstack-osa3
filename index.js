41ddeb72172b3949118b89d6eedee9a5ea40a502
Inka Häkkänen <inkahakkanenn@gmail.com>
2024-03-15 14:43:37 +0200

    Validation error frontend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+
 const Person = require('./models/person')
 
 morgan.token('content-body', req => {
